f6b668bf4c1f368659570e1f84d3b49b92214613
Kalbskinder <mimosc007@gmail.com>
2026-06-17 13:27:07 +0200

    Update command sender block to avoid confusion

--- src/lib/codegen/generators/java/events/command_sender_get.ts
+++ src/lib/codegen/generators/java/events/command_sender_get.ts
@@ -3,7 +3,7 @@ import { Order } from '../../java.js';
 
 export default {
     block: 'command_sender_get',
-    generator: function(block: Blockly.Block) {
+    generator: function(_block: Blockly.Block) {
         return ['ctx.getSender()', Order.ATOMIC];
     }
 }
