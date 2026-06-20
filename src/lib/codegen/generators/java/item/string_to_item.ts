999980e66a240e9d37eeebf5bc499d8f79486d6b
Kalbskinder <mimosc007@gmail.com>
2026-06-20 10:52:06 +0200

    Added translation fo string to item block

---
+++ src/lib/codegen/generators/java/item/string_to_item.ts
@@ -0,0 +1,15 @@
+import * as Blockly from 'blockly';
+import { Order, imports } from '../../java.js';
+
+export default {
+  block: 'string_to_item',
+  generator: function(block: Blockly.Block) {
+    const itemString = block.getFieldValue('ITEM') || '';
+    const item = itemString.trim().substring(itemString.indexOf(':') + 1).toUpperCase();
+    const code = `Helpers.itemHelper.newItem().material(Material.${item}).amount(1).build()`;
+
+    imports.add('import org.bukkit.Material;');
+
+    return [code, Order.ATOMIC];
+  },
+};
