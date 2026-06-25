a1110827d748f8761cf07d5c9c7235e978690106
Kalbskinder <mimosc007@gmail.com>
2026-06-25 19:46:35 +0200

    Fix wrong block translation for teleport player block

--- src/lib/codegen/generators/java/teleport/teleport_player_location.ts
+++ src/lib/codegen/generators/java/teleport/teleport_player_location.ts
@@ -4,9 +4,9 @@ import { JavaGenerator, Order } from '../../java.js';
 export default {
   block: 'teleport_player_location',
   generator: function(block: Blockly.Block) {
-    const player = JavaGenerator.valueToCode(block, 'PLAYER', Order.ATOMIC);
-    const location = JavaGenerator.valueToCode(block, 'LOCATION', Order.ATOMIC);
-    const code = `teleportPlayerHelper.teleportEntity(${player}, ${location});\n`;
+    const player = JavaGenerator.valueToCode(block, 'PLAYER', Order.ATOMIC) || 'null';
+    const location = JavaGenerator.valueToCode(block, 'LOCATION', Order.ATOMIC) || 'null';
+    const code = `Helpers.teleportHelper.teleportEntity(${player}, ${location});\n`;
     return code;
   },
 };
