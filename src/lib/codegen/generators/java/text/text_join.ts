9726adf80ed1a8ab199fcbfebadbb42f0d483389
Kalbskinder <mimosc007@gmail.com>
2026-07-09 20:11:21 +0200

    Fix text_join breaking when no inputs provided

--- src/lib/codegen/generators/java/text/text_join.ts
+++ src/lib/codegen/generators/java/text/text_join.ts
@@ -6,9 +6,18 @@ export default {
   generator: function(block: Blockly.Block) {
     const pieces = [];
     for (let i = 0; i < block.inputList.length; i++) {
-        pieces.push(JavaGenerator.valueToCode(block, 'ADD' + i, Order.ATOMIC));
+        const inputName = 'ADD' + i;
+        if (block.getInput(inputName)) {
+            pieces.push(JavaGenerator.valueToCode(block, inputName, Order.ATOMIC));
+        }
     }
-    const code = 'String.join("", ' + pieces.join(', ') + ')';
+    
+    let code: string;
+    if (pieces.length === 0) {
+        code = '""';
+    } else {
+        code = 'String.join("", ' + pieces.join(', ') + ')';
+    }
     return [code, Order.ATOMIC];
   },
 };
